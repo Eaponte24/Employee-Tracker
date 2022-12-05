7d5d89a74251aa910f95246996c67be413c92919
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-12-04 23:32:49 -0500

    added first questions and the functions that will be ran when they are chosed to the server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cTable = require('console.table');
+const inquirer = require('inquirer'); 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,7 +18,77 @@ const db = mysql.createConnection(
     user: 'root',
     // MySQL password
     password: 'Winter24!',
-    database: 'movie_db'
+    database: 'workers_db'
   },
-  console.log(`Connected to the movie_db database.`)
+  console.log(`Connected to the workers_db database.`)
 );
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + db.threadId);
+    DBconnected();
+  });
+  
+  // once connected a 
+  DBconnected = () => {
+    
+    console.log("* ------------------------------- *")
+    console.log("*        Workers MANAGER          *")
+    console.log("* ------------------------------- *")
+   
+    promptUserquestions();
+  };
+
+
+const promptUserquestions = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'choices', 
+        message: 'What would you like to do?',
+        choices: ['View all departments', 
+                  'View all roles', 
+                  'View all employees', 
+                  'Add a department', 
+                  'Add a role', 
+                  'Add an employee', 
+                  'Update an employee role',
+                  'No Action']
+      }
+    ])
+      .then((answers) => {
+        const { choices } = answers; 
+  
+        if (choices === "View all departments") {
+          showDepartments();
+        }
+  
+        if (choices === "View all roles") {
+          showRoles();
+        }
+  
+        if (choices === "View all employees") {
+          showEmployees();
+        }
+  
+        if (choices === "Add a department") {
+          addDepartment();
+        }
+  
+        if (choices === "Add a role") {
+          addRole();
+        }
+  
+        if (choices === "Add an employee") {
+          addEmployee();
+        }
+  
+        if (choices === "Update an employee role") {
+          updateEmployee();
+        }
+  
+        if (choices === "No Action") {
+          db.end()
+      };
+    });
+  };
